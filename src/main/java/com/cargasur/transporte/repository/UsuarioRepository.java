4b54c98924275cbc5acd407ff2ec0d645ba9be9e
Fernanda Isidora Aliaga González <extfaliaga@transbank.cl>
2025-06-04 15:55:53 -0400

    fix

--- src/main/java/com/cargasur/transporte/repository/UsuarioRepository.java
+++ src/main/java/com/cargasur/transporte/repository/UsuarioRepository.java
@@ -4,6 +4,8 @@ import com.cargasur.transporte.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByCorreoAndClave(String correo, String clave);
